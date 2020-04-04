8fa0d25c29cb2a51550358a07e850eb123517e3b
Rajaguru <rajaguru@onedatasoftware.com>
2020-04-04 22:46:41 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const hbs = require("hbs");
+const path = require("path");
+const fs = require("fs");
+var app = express();
+
+hbs.registerPartials(__dirname + "/views/partials/");
+app.set("view engine", "hbs");
+
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFileSync("server.log", log + "\n");
+  next();
+});
+// app.use((req, res, next) => {
+//   res.render("maintainance.hbs");
+//   next();
+// });
+
+app.use(express.static(__dirname + "/public/"));
+
+hbs.registerHelper("getCurrentYear", () => new Date().getFullYear());
+
+hbs.registerHelper("screamIt", (text) => text.toUpperCase());
+
+app.get("/", (req, res) => {
+  res.render("home.hbs", {
+    pageTitle: "Home Page",
+    welcomeMessage: "Welcome to My WebSite",
+    currentYear: new Date().getFullYear(),
+  });
+});
+app.get("/about", (req, res) => {
+  res.render("about.hbs", {
+    pageTitle: "About Page",
+    currentYear: new Date().getFullYear(),
+  });
+});
+app.listen(3000);
